0812350ebabbc0880ca794fbd16b1088c25c6ccc
ehsan <e.kamalloo@gmail.com>
2023-11-28 14:00:31 -0500

    typo fixed

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -5,15 +5,15 @@ NC='\033[0m' # No Color
 
 ANNOTATE_FILE=$1
 
-for model_output in `ls data/model_output/NQ_*.jsonl`; do
+for model_output in `ls data/model_outputs/NQ_*.jsonl`; do
   echo "${Green}evaluating $model_output ...${NC}"
   python -m qaeval $model_output --annotation $ANNOTATE_FILE
 done
 
-model_output="data/model_output/NQ301_text-davinci-003_zeroshot.jsonl"
+model_output="data/model_outputs/NQ301_text-davinci-003_zeroshot.jsonl"
 echo "${Green}evaluating $model_output ...${NC}"
 python -m qaeval $model_output --annotation $ANNOTATE_FILE
 
-model_output="data/model_output/NQ301_text-davinci-003_fewshot-n64.jsonl"
+model_output="data/model_outputs/NQ301_text-davinci-003_fewshot-n64.jsonl"
 echo "${Green}evaluating $model_output ...${NC}"
 python -m qaeval $model_output --annotation $ANNOTATE_FILE
