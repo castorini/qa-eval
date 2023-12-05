d40975fa65024b74a2abec3f6c34af81f40358fe
ehsan <e.kamalloo@gmail.com>
2023-12-04 20:26:55 -0500

    new scripts added

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
+# scripts/eval_all.sh "0" 0 prompts/eval-v0.2-few-shot_chat.txt google/flan-t5-base 3
+
 Green='\033[0;32m'
 NC='\033[0m' # No Color
 
-ANNOTATE_FILE=$1
+CUDA_VISIBLE_DEVICES=$1
+GROUP=$2
+PROMPT=$3
+MODEL=$4
+NUM_SAMPLES=$5
 
-for model_output in `ls data/model_outputs/NQ_*.jsonl`; do
-  echo -e "${Green}Evaluating $model_output ...${NC}"
-  python -m qaeval $model_output --annotation $ANNOTATE_FILE --overwrite
+cnt=0
+for model_output in `ls data/model_outputs/NQ301_*.jsonl`; do
+  if [ `expr $cnt % 2` == $GROUP ]
+  then
+    echo -e "${Green}Evaluating $model_output ...${NC}"
+    python -m qaeval $model_output --prompt $PROMPT --model $3 --do_greedy --num_beams 10 --num_samples $NUM_SAMPLES --batch_size 1
+  else
+    echo -e "${Green}Evaluating $model_output ...${NC}"
+    python -m qaeval $model_output --prompt $PROMPT --model $3 --do_greedy --num_beams 10 --num_samples $NUM_SAMPLES --batch_size 1
+  fi
+  ((cnt++))
 done
 
-model_output="data/model_outputs/NQ301_text-davinci-003_zeroshot.jsonl"
-echo -e "${Green}Evaluating $model_output ...${NC}"
-python -m qaeval $model_output --annotation $ANNOTATE_FILE --overwrite
-
-model_output="data/model_outputs/NQ301_text-davinci-003_fewshot-n64.jsonl"
-echo -e "${Green}Evaluating $model_output ...${NC}"
-python -m qaeval $model_output --annotation $ANNOTATE_FILE --overwrite
---
+++ scripts/subset_creation.py
@@ -0,0 +1,58 @@
+import argparse
+import json
+from pathlib import Path
+
+# Define the file paths
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "predict_file",
+    type=str,
+    help="Path to a predict file",
+)
+parser.add_argument(
+    "--ref_file",
+    type=str,
+    default="data/model_outputs/NQ301_text-davinci-003_zeroshot.jsonl",
+    help="Path to a reference predict file",
+)
+
+args = parser.parse_args()
+
+# file1_path = "../data/model_outputs/NQ301_text-davinci-003_zeroshot.jsonl"
+# file2_path = "../data/model_outputs/NQ_Rocketv2_FiD.jsonl"
+# output_path = "../data/model_outputs/NQ301_Rocketv2_FiD.jsonl"  # Define the output file path
+
+# Create a set to store questions from file1.jsonl
+questions_set = set()
+
+# Read questions from file1.jsonl and store them in the set
+ref_file = Path(args.ref_file)
+with open(ref_file) as f:
+    for line in f:
+        if not line.strip():
+            continue
+
+        item = json.loads(line)
+        if "question" in item:
+            questions_set.add(item["question"])
+
+# Create a list to store JSON objects from file2.jsonl where the question is in the set
+subset = []
+
+# Read file2.jsonl and extract questions that are in the set
+predict_file = Path(args.predict_file)
+with open(predict_file) as f:
+    for line in f:
+        if not line.strip():
+            continue
+
+        item = json.loads(line)
+        if "question" in item and item["question"] in questions_set:
+            subset.append(item)
+
+output_file = predict_file.stem.split("_")[0] + "_" + "_".join(ref_file.name.split("_")[1:])
+output_path = predict_file.parent / output_file
+# Save the extracted subset to a new JSONL file
+with open(output_path, mode='w') as writer:
+    for item in subset:
+        writer.write(json.dumps(item) + "\n")
