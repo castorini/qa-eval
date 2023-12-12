86dc0a09ffc683cd159188b1df9990bb096e1a6b
ehsan <e.kamalloo@gmail.com>
2023-12-12 17:13:59 -0500

    more stats added

--- scripts/agreement.py
+++ scripts/agreement.py
@@ -5,14 +5,36 @@ from pathlib import Path
 from nltk.metrics import agreement
 from tqdm import tqdm
 
+from qaeval import SimpleTokenizer
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "file_pattern",
     type=str,
     help="File pattern",
 )
+parser.add_argument(
+    "--analysis_file",
+    type=str,
+    default=None,
+    help="Path to analysis file",
+)
 
 args = parser.parse_args()
+
+tokenizer = SimpleTokenizer()
+
+analytics = {}
+if args.analysis_file:
+    with open(args.analysis_file) as f:
+        reader = csv.DictReader(f, delimiter="\t")
+
+    for row in reader:
+        q = tokenizer.tokenize(row["Question"], as_string=True).lower()
+        p = tokenizer.tokenize(row["Predicted answer"], as_string=True)
+        reason = row["Why-Hu"].strip()
+        if reason:
+            analytics[f"{q}###{p}"] = reason
 
 file_pattern = Path(args.file_pattern)
 results_dir = file_pattern.parent
@@ -20,6 +42,7 @@ results_dir = file_pattern.parent
 annotations = []
 judg1_counts = defaultdict(int)
 judg1_ratios = defaultdict(int)
+judg1_per_reason = defaultdict(lambda: defaultdict(int))
 for i, results_file in tqdm(enumerate(results_dir.glob(file_pattern.name)), colour="green"):
     with open(results_file) as f:
         reader = csv.reader(f, delimiter="\t")
@@ -35,13 +58,21 @@ for i, results_file in tqdm(enumerate(results_dir.glob(file_pattern.name)), colo
             for j, judg in enumerate(judgments):
                 annotations.append((f"c{j}", f"{results_file.stem}#{r}", judg))
 
+            tok_q = tokenizer.tokenize(question, as_string=True).lower()
+            tok_p = tokenizer.tokenize(prediction, as_string=True)
+            reason = analytics.get(f"{tok_q}###{tok_p}", None)
+            if reason:
+                judg1_per_reason[reason][judged_ones] += 1
 
 print(f"{len(annotations)} collected from {len(list(results_dir.glob(file_pattern.name)))} result files")
 
 task = agreement.AnnotationTask(data=annotations)
 print("Fleiss' Kappa =", task.multi_kappa())
 
+print()
 print("***" * 30)
+print()
+
 for freq in sorted(judg1_counts.keys()):
     print(
         f"#judged {freq} times as one = {judg1_counts[freq]} "
@@ -50,8 +81,26 @@ for freq in sorted(judg1_counts.keys()):
 
 print()
 print("***" * 30)
+print()
+
 for ratio in sorted(judg1_ratios.keys()):
     print(
         f"#judged {ratio} times as one = {judg1_ratios[ratio]} "
         f"({100. * judg1_ratios[ratio] / sum(judg1_ratios.values()):.1f}%)"
     )
+
+print()
+print("***" * 30)
+print()
+
+for reason, counts in judg1_per_reason.items():
+    reason_percent = 100. * sum(counts.values()) / sum(judg1_counts.values())
+    for freq in sorted(counts.keys()):
+        print(
+            f"[{reason} ({reason_percent:.1f}%)]"
+            f"#judged {freq} times as one = {counts[freq]} "
+            f"({100. * counts[freq] / sum(counts.values()):.1f}%)"
+        )
+
+print()
+print("***" * 30)
